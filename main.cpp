aaa9b1a8ae5058e4545c425bd6342b426ec0cf24
wowyomad <vadimsun8837@gmail.com>
2022-04-06 02:08:18 +0300

    A lot of changes, except Zadanie

--- main.cpp
+++ main.cpp
@@ -10,82 +10,58 @@ struct List
 };
 
 void CreateList(List **beg, List **end, const int data = 0);
-
-void PushBack(List **end, const int data = 0);
-void PushFront(List **beg, const int data = 0);
 void Push(List **list, const int data = 0);
-void Pop(List **list, const int data = 0);
-void PopBack(List **end, int data);
-void PopFront(List **beg, int data);
-
-void PrintBack(List *end);
-void PrintFront(List *beg);
-
+void Pop(List **list, int *data = NULL);
+void Print(List *list);
 void Clear(List **list);
 
 void Zadanie(List **list);
 
 int main(int argc, char **arv)
 {
-
     List *beg;
     List *end;
     CreateList(&beg, &end, 5);
-    PushBack(&end, 10);
-    PushFront(&beg, 15);
-    PrintBack(end);
-    PrintFront(beg);
+    Push(&beg, 10);
+    Push(&end, 15);
+    Print(beg);
+    Print(end);
+    Pop(&end);
+    Print(beg);
+    Print(end);
     Clear(&beg);
     std::cout << "End\n";
     getchar();
 }
 
-void PrintBack(List *end)
+void Print(List* list)
 {
-    List *temp = end;
-    while (temp != NULL)
+    List *temp = list;
+    if(list->prev == NULL)
     {
-        printf("%d%c", temp->data, ' ');
-        temp = temp->next;
+        while(temp != NULL)
+        {
+             printf("%d%c", temp->data, ' ');
+             temp = temp->next;
+        }
+    }
+    else
+    {
+        while(temp != NULL)
+        {
+             printf("%d%c", temp->data, ' ');
+             temp = temp->prev;
+        }
     }
     printf("%c", '\n');
 }
 
-void PrintFront(List *beg)
-{
-    List *temp = beg;
-    while (temp != NULL)
-    {
-        printf("%d%c", temp->data, ' ');
-        temp = temp->prev;
-    }
-    printf("%c", '\n');
-}
 void CreateList(List **beg, List **end, const int data)
 {
     List *temp = new List;
     temp->data = data;
     temp->next = temp->prev = NULL;
     *beg = *end = temp;
-}
-
-void PushBack(List **end, const int data)
-{
-    if (*end == NULL)
-    {
-        return;
-    }
-    List *temp = new List{data, NULL, *end};
-    (*end)->prev = temp;
-    *end = temp;
-}
-
-void PushFront(List **beg, const int data)
-{
-
-    List *temp = new List{data, *beg, NULL};
-    (*beg)->next = temp;
-    *beg = temp;
 }
 
 void Push(List **list, const int data)
@@ -98,7 +74,6 @@ void Push(List **list, const int data)
 
     if ((*list)->prev == NULL)
     {
-
         temp->prev = NULL;
         temp->next = *list;
         (*list)->prev = temp;
@@ -110,6 +85,32 @@ void Push(List **list, const int data)
         temp->prev = *list;
         (*list)->next = temp;
         *list = temp;
+    }
+}
+
+void Pop(List **list, int *data)
+{
+    if (*list == NULL)
+        return;
+
+    if (data != NULL)
+        *data = (*list)->data;
+
+    List *temp;
+
+    if ((*list)->prev == NULL)
+    {
+        temp = (*list)->next;
+        temp->prev = NULL;
+        delete *list;
+        (*list) = temp;
+    }
+    else
+    {
+        temp = (*list)->prev;
+        temp->next = NULL;
+        delete *list;
+        (*list) = temp;
     }
 }
 
@@ -146,7 +147,7 @@ void Zadanie(List **list)
     }
     avg /= count;
     List *temp1 = NULL;
-    PushBack(list);
+    Push(list);
     temp = *list;
     while (temp->next != NULL)
     {
